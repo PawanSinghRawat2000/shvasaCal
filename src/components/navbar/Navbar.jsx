774db160c8004459f429842d31655d15bb9c06e9
PawanSinghRawat2000 <88731967+PawanSinghRawat2000@users.noreply.github.com>
2024-11-19 12:13:38 +0530

    fix

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -14,7 +14,8 @@ function Navbar({ syncWithGoogle,setSyncWithGoogle}) {
         navigate('/login');
     }
     const handleSignIn = async() => {
-        window.location.href = `${import.meta.env.VITE_BACKEND_URL}/googleAuth`;
+        // window.location.href = `${import.meta.env.VITE_BACKEND_URL}/googleAuth`;
+        const response = await getData("googleAuth");
     }
     const handleSignout=()=>{
         document.cookie = "google_sync_token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; SameSite=Lax";
